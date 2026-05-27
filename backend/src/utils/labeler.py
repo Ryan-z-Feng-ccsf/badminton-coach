c6cfe12c5295015d7bff1b8a2fafd5c0b9f83dad
Ryan-z-Feng-ccsf <zfeng57@mail.ccsf.edu>
2026-05-27 09:52:17 -0700

    feat: implement the logic for the labeler

--- backend/src/utils/labeler.py
+++ backend/src/utils/labeler.py
@@ -14,3 +14,27 @@ from src.utils.extractor import PoseDetector, VideoCapture
 MODEL_PATH = config.get_path("POSE_MODEL_PATH")
 DATA_VIDEO_PATH = config.get_path("RAW_VIDEO_PATH")
 SAVE_DIR = config.get_path("TRAIN_LUNGE_DIR")
+
+os.makedirs(SAVE_DIR, exist_ok=True)
+
+def runAutomatedLabeler():
+    buffer = deque(maxlen=11)
+    
+    with PoseDetector(MODEL_PATH) as pose_detector:
+        with VideoCapture(DATA_VIDEO_PATH) as video_capture:
+            frame_idx = 0
+            print("🕹️")
+            
+            while True:
+                success, frame_bgr = video_capture.cap.read()
+                if not success:
+                    print("Video capture failed / end of video")
+                    break
+
+                result = pose_detector.process_frame(frame_idx, frame_bgr, video_capture.get_fps())
+                buffer.append(result)
+                
+                
+                
+                
+                frame_idx += 1
